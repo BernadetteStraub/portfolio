09a04ebb2e722c17d629c20049c70dddc0275199
Bernadette Straub <str.bernadette@gmail.com>
2026-01-24 18:43:43 +0200

    update about

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -2,19 +2,18 @@
 	<h2 class="my-4 text-2xl text-error-400">> About Me</h2>
 	<p class="text-justify text-base text-secondary-400">
 		I'm a Fullstack dev currently located in <span class="text-error-400">Zürich, Switzerland</span
-		>, whose focus lies in backend development, as you can tell from my simple portfolio. 
-        My passion for coding started from a young age when I began dabbling with HTML and
-		CSS, however I ended up studying for an economics degree because I found interest in that field. 
-        Later on I switched studies and graduated with a
-		<span class="text-error-400">Bsc in Business Informatics</span>, which combined both of the
-		things I enjoy, on top of allowing more space for creativity. Throughout my university studies I
-		also completed a 5 month
+		>, with hands-on experience in both frontend and backend development. 
+        My passion for programming started from a young age when I began experimenting with HTML and
+		CSS. I initially pursued a degree in economics due to my interest in analytical problem-solving, 
+		but later transitioned to <span class="text-error-400">Business Informatics</span>, 
+		which allowed me to combine technical skills with creativity. 
+		Throughout my university studies I also completed a 5 month
 		<a
 			href="https://everyonecodes.io/"
 			target="_blank"
 			rel="noopener noreferrer"
 			class="text-error-400 hover:text-primary-400">Spring Boot programme</a
-		> in Vienna, which led me to my first job as a Software Developer.
+		> in Vienna, which led me to my first role as a Software Developer.
 	</p>
 </section>
 
